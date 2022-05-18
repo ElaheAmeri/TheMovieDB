862d0e69b5b12288463e783bc1b3e08c77e412cd
Elahe Ameri <elahelastname@gmail.com>
2022-05-18 11:31:21 -0700

    add retrofit and Api service

--- app/src/main/java/com/example/themoviedb/network/MovieApiService.kt
+++ app/src/main/java/com/example/themoviedb/network/MovieApiService.kt
@@ -1,4 +1,35 @@
 package com.example.themoviedb.network
 
-class MovieApiService {
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+
+private const val BASE_URL =
+    "https://developers.themoviedb.org"
+
+private val retrofit = Retrofit.Builder()
+
+object MovieApi {
+    val retrofitService : MovieApiService by lazy {
+        retrofit.create(MovieApiService::class.java) }
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
 }
+
+
+class MovieApiService {
+
+
+    interface MovieApiService {
+        @GET("photos")
+       suspend fun getPhotos()
+    }
+}
+
+
+
+
